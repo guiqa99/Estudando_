665acb68dfac35fb3979cee3e41bfdee35452fa7
Guilherme Queiroz <guiloucoporjesus99@gmail.com>
2023-10-09 17:04:50 -0300

    first commit

---
+++ javaScript/Operadores/Aritméticos.js
@@ -0,0 +1,27 @@
+// operadores: +, -, *, /, %, ** 
+//  operando: numbers
+
+// 5 + 2
+// 5 - 2
+// 5 * 2
+// 5 / 2
+// 5 % 2 resto da divisão inteira (resto)
+// 5 ** 2 potência de um número
+
+// Ordem de Precendência:
+// 1. Parênteses ()
+// 2. Exponenciação **
+// 3. Multiplicação e Divisão (*,/,%)
+// 4. Adição e Subtração (+,-)
+
+// var a = 5 + 3
+// var b = a % 5 
+// var c = 5 * b ** 2
+// var d = 10 - a / 2
+// var e = 6 * 2 / d
+// var f = b % e + 4 / 2 
+
+// ++n
+// n++
+// --n
+// n--
