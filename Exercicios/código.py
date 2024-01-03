9c7749bc7d3d35fd4b0cc89b1a32f9baaaa9f987
Guilherme Queiroz <guiloucoporjesus99@gmail.com>
2024-01-03 16:30:31 -0300

    codigo password mais  limpo

--- Exercicios/código.py
+++ Exercicios/código.py
@@ -4,12 +4,10 @@ pass1 = (['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
         'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' 'ç',
         'z' 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4',
         '5', '6', '7', '8', '9', '0'])
-        #  , '/', ',', '.', '-',)
-        # '+', '*', '/', '&', '@', '!', '#', '$', '%', '¨']
 
 password = ""
 
 for x in range(6):
     password = password + random.choices(pass1)[0]
 
-print('Sua nova senha:\n', password)
+print('Sua nova senha:',password)
