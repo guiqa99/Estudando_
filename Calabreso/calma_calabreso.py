41da5cf9ab7f30f4297a98c6008015e482a0a932
Guilherme99 <guiloucoporjesus99@gmail.com>
2024-02-28 17:05:27 -0300

    new

--- Calabreso/calma_calabreso.py
+++ Calabreso/calma_calabreso.py
@@ -1,13 +1,24 @@
-palavra = str(input('Digite um nome: '))
+# palavra = str(input('Digite um nome: '))
 
-def calabreso():
+# def calabreso():
   
-  if palavra[-1] == 'a':
-    palavra_nova = palavra[:-1] + 'o'
-    return f'Calma {palavra_nova}'
-  else:
-    palavra_nova = palavra + 'o'
-    return f'Calma {palavra_nova}'
+#   if palavra[-1] == 'a':
+#     palavra_nova = palavra[:-1] + 'o'
+#     return f'Calma {palavra_nova}'
+#   else:
+#     palavra_nova = palavra + 'o'
+#     return f'Calma {palavra_nova}'
   
-msg = calabreso()
+# msg = calabreso()
+# print(msg)
+
+def calabreso(palavra):
+    if palavra[-1] == 'a':
+        nova_palavra = "".join([palavra[:-1], 'o'])  # Substitui o último 'a' por 'o'
+    else:
+        nova_palavra = "".join([palavra, 'o'])  # Adiciona 'o' ao final da palavra
+    return f'Calma {nova_palavra}'  # Retorna a mensagem com a nova palavra
+
+palavra = input('Digite um nome: ')
+msg = calabreso(palavra)
 print(msg)
